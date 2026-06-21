478ca3e30244cb834587bb140fd233a7587bf9a9
jackcylin <jackcylin@gmail.com>
2026-06-21 18:57:53 +0800

    chore: scaffold @hiko/signin-headless

---
+++ tsup.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig([
+  { entry: ["src/index.js", "src/element.js"], format: ["esm"], dts: true, clean: true, external: ["lit", "@hiko/signin-widget"] },
+  { entry: { "hiko-signin-headless": "src/element.js" }, format: ["iife"], globalName: "HikoSigninHeadless", noExternal: ["lit", "@hiko/signin-widget"], minify: true, outDir: "dist" },
+]);
